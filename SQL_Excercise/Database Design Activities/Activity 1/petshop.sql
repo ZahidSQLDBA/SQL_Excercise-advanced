0b3ebeba8cf9e808c510118e4d7b6a869571fba1
chounhlee <chounhlee@gmail.com>
2020-12-26 00:51:36 -0500

    Activity 1 Complete

--- SQL_Excercise/Database Design Activities/Activity 1/petshop.sql
+++ SQL_Excercise/Database Design Activities/Activity 1/petshop.sql
@@ -1,71 +1,95 @@
-DROP DATABASE IF EXISTS petshop;
-CREATE DATABASE petshop;
-USE petshop;
+/*
+Activity 1
+Deign a database for a pet shop. It must include:
+
+- Information about the doctors in the practice, including name, type of pets they treat, and when they joined the practice
+- Pet owner information, including name, mailing address, phone number, and email address
+- Pet information, including the name of the pet, who owns the pet, what kind of pet it is, its birthdate, and its vaccination records.
+*/
+-- Pet shop does not seem appropriate name. Will adjust and name it AnimalClinic
+
+DROP DATABASE IF EXISTS AnimalClinic;
+CREATE DATABASE AnimalClinic;
+USE AnimalClinic;
 
 DROP TABLE IF EXISTS doctor;
 CREATE TABLE doctor(
 	doctorId INT AUTO_INCREMENT,
     CONSTRAINT pk_doctor
 		primary key (doctorId),
-	albumTitle VARCHAR(100) NOT NULL,
-    label VARCHAR(50),
+	doctorFirstName VARCHAR(50) NOT NULL,
+    doctorLastName VARCHAR(50) NOT NULL,
     joinDate DATE
 );
 
-DROP TABLE IF EXISTS artist;
-CREATE TABLE artist(
-	artistId INT AUTO_INCREMENT,
-    CONSTRAINT pk_artist
-		primary key (artistId),
-	artistFirstName varchar(25) NOT NULL,
-    artistLastName varchar(50)
+DROP TABLE IF EXISTS petSpecies;
+CREATE TABLE petSpecies(
+	petSpeciesId INT AUTO_INCREMENT,
+    CONSTRAINT pk_petSpecies
+		primary key (petSpeciesId),
+	Species varchar(50) NOT NULL
 );
 
-DROP TABLE IF EXISTS band;
-CREATE TABLE band(
-	bandId INT AUTO_INCREMENT,
-    CONSTRAINT pk_band
-		primary key (bandId),
-	bandName VARCHAR(50)
+DROP TABLE IF EXISTS petOwner;
+CREATE TABLE petOwner(
+	petOwnerId INT AUTO_INCREMENT,
+    CONSTRAINT pk_petOwner
+		primary key (petOwnerId),
+	ownerFirstName VARCHAR(50),
+    ownerLastName VARCHAR(50),
+    address VARCHAR(200),
+    phone int,
+    email VARCHAR(50)
 );
 
-DROP TABLE IF EXISTS bandArtist;
-CREATE TABLE bandArtist (
-	bandId INT,
-    artistId INT,
-    constraint pk_bandArtist
-		PRIMARY KEY (bandId, artistId),
-	constraint pk_bandArtist_band
-		foreign key (bandId)
-		references band(bandId),
-	constraint pk_bandArtist_artist
-		foreign key (artistId)
-		references artist(artistId)
+DROP TABLE IF EXISTS vaccine;
+CREATE TABLE vaccine(
+	vaccineId INT AUTO_INCREMENT,
+    CONSTRAINT pk_vaccine
+		primary key (vaccineId),
+	vaccineName varchar(50) NOT NULL
 );
 
-DROP TABLE IF EXISTS song;
-CREATE TABLE song (
-	songId INT NOT NULL auto_increment,
-    songTitle VARCHAR(100) NOT NULL,
-    videoUrl VARCHAR(100),
-    bandId INT NOT NULL,
-	constraint pk_song
-		PRIMARY KEY (songId),
-	constraint fk_song_band
-		FOREIGN KEY (bandId)
-        REFERENCES band(bandId)
+DROP TABLE IF EXISTS doctorPetSpecialty;
+CREATE TABLE doctorPetSpecialty(
+	doctorId INT,
+    petSpeciesId INT,
+    constraint pk_doctorPetSpecialty
+		PRIMARY KEY (doctorId, petSpeciesId),
+	constraint pk_doctorPetSpecialty_doctor
+		foreign key (doctorId)
+		references doctor(doctorId),
+	constraint pk_doctorPetSpecialty_petSpecies
+		foreign key (petSpeciesId)
+		references petSpecies(petSpeciesId)
 );
 
-DROP TABLE IF EXISTS songAlbum;
-CREATE TABLE songAlbum (
-	songId INT,
-    albumID INT,
-    constraint pk_songAlbum
-		PRIMARY KEY (songId, albumId),
-	constraint pk_songAlbum_song
-		foreign key (songId)
-		references song(songId),
-	constraint pk_songAlbum_album
-		foreign key (albumId)
-		references album(albumId)
+DROP TABLE IF EXISTS pet;
+CREATE TABLE pet (
+	petId INT NOT NULL auto_increment,
+    petName VARCHAR(50) NOT NULL,
+    petOwnerId INT,
+    petSpeciesId INT,
+	constraint pk_pet
+		PRIMARY KEY (petId),
+	constraint fk_pet_petOwner
+		FOREIGN KEY (petOwnerId)
+        REFERENCES petOwner(petOwnerId),
+	constraint fk_pet_petSpecies
+		FOREIGN KEY (petSpeciesId)
+        REFERENCES petSpecies(petSpeciesId)
+);
+
+DROP TABLE IF EXISTS petVaccineRecord;
+CREATE TABLE petVaccineRecord (
+	petId INT,
+    vaccineId INT,
+    constraint petVaccineRecord
+		PRIMARY KEY (petId, vaccineId),
+	constraint pk_petVaccineRecord_pet
+		foreign key (petId)
+		references pet(petId),
+	constraint pk_petVaccineRecord_vaccine
+		foreign key (vaccineId)
+		references vaccine(vaccineId)
 );
